9bcd3afddf5f8c05615f8fb7c2d27ccd77b197b0
Bohdan Psiuk <151725827+PythonWizardry@users.noreply.github.com>
2025-10-01 14:45:53 +0300

    Adding code for all request for Swagger UI

--- Lab_4-5/flask_project/app/my_project/auth/route/orders/review_route.py
+++ Lab_4-5/flask_project/app/my_project/auth/route/orders/review_route.py
@@ -68,11 +68,56 @@ def create_review() -> Response:
 @review_bp.get('/<int:review_id>')
 def get_review(review_id: int) -> Response:
 
+    """
+    Get a review by ID
+    ---
+    parameters:
+      - in: path
+        name: review_id
+        required: true
+        schema:
+          type: integer
+        description: The ID of the review
+    responses:
+      200:
+        description: Returns the review
+    """
+
     return make_response(jsonify(review_controller.find_by_id(review_id)), HTTPStatus.OK)
 
 
 @review_bp.put('/<int:review_id>')
 def update_review(review_id: int) -> Response:
+
+    """
+    Update a review by ID
+    ---
+    consumes:
+      - application/json
+    parameters:
+      - in: path
+        name: review_id
+        required: true
+        schema:
+          type: integer
+        description: The ID of the review
+      - in: body
+        name: body
+        required: true
+        schema:
+          type: object
+          properties:
+            guest_id:
+              type: integer
+            review_context:
+              type: string
+          required:
+            - guest_id
+            - review_context
+    responses:
+      200:
+        description: Review updated
+    """
 
     content = request.get_json()
     review = Review.create_from_dto(content)
@@ -83,6 +128,28 @@ def update_review(review_id: int) -> Response:
 @review_bp.patch('/<int:review_id>')
 def patch_review(review_id: int) -> Response:
 
+    """
+    Patch a review by ID
+    ---
+    consumes:
+      - application/json
+    parameters:
+      - in: path
+        name: review_id
+        required: true
+        schema:
+          type: integer
+        description: The ID of the review
+      - in: body
+        name: body
+        required: true
+        schema:
+          type: object
+    responses:
+      200:
+        description: Review updated
+    """
+
     content = request.get_json()
     review_controller.patch(review_id, content)
     return make_response("Review updated", HTTPStatus.OK)
@@ -90,13 +157,33 @@ def patch_review(review_id: int) -> Response:
 
 @review_bp.delete('/<int:review_id>')
 def delete_review(review_id: int) -> Response:
-
+    """
+    Delete a review by ID
+    ---
+    parameters:
+      - in: path
+        name: review_id
+        required: true
+        schema:
+          type: integer
+        description: The ID of the review
+    responses:
+      200:
+        description: Review deleted
+    """
     review_controller.delete(review_id)
     return make_response("Review deleted", HTTPStatus.OK)
 
 
 @review_bp.delete('')
 def delete_reviews_all() -> Response:
+    """
+    Delete all reviews
+    ---
+    responses:
+      200:
+        description: All reviews deleted
+    """
     review_controller.delete_all()
     return make_response("All reviews deleted", HTTPStatus.OK)
 
