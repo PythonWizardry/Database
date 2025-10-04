14d616486bed9ac817ea2ee58c21f287413f5f01
PythonWizardry <bahdanuch@gmail.com>
2025-10-04 13:57:45 +0000

    i hope this finally will be without correct yamal file and venv

--- Lab_4-5/flask_project/app/app.py
+++ Lab_4-5/flask_project/app/app.py
@@ -13,7 +13,7 @@ import yaml
 from flask_project.app.my_project import create_app
 
 DEVELOPMENT_PORT = 5000
-PRODUCTION_PORT = 8080
+PRODUCTION_PORT = 5000
 HOST = "0.0.0.0"
 DEVELOPMENT = "development"
 PRODUCTION = "production"
@@ -22,7 +22,7 @@ ADDITIONAL_CONFIG = "ADDITIONAL_CONFIG"
 
 if __name__ == '__main__':
     flask_env = os.environ.get(FLASK_ENV, DEVELOPMENT).lower()
-    config_yaml_path = os.path.join(os.getcwd(), 'config', 'app.yml')
+    config_yaml_path = os.path.join(os.getcwd(), 'config', 'app_ec2.yml')
 
     with open(config_yaml_path, "r", encoding='utf-8') as yaml_file:
         config_data_dict = yaml.load(yaml_file, Loader=yaml.FullLoader)
@@ -30,7 +30,7 @@ if __name__ == '__main__':
 
         if flask_env == DEVELOPMENT:
             config_data = config_data_dict[DEVELOPMENT]
-            create_app(config_data, additional_config).run(port=DEVELOPMENT_PORT, debug=True)
+            create_app(config_data, additional_config).run(host="0.0.0.0", port=5000, debug=True)
 
         elif flask_env == PRODUCTION:
             config_data = config_data_dict[PRODUCTION]
