8210fb1bc2f9b5dd291e215623a85a3799e4d6ed
Bohdan Psiuk <151725827+PythonWizardry@users.noreply.github.com>
2025-11-12 16:22:05 +0200

    Changes to app.py and app.yml in order to get confidentical information from AWS Secret Manadger to connect to RDS

--- Lab_4-5/flask_project/app/app.py
+++ Lab_4-5/flask_project/app/app.py
@@ -9,6 +9,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 
 from waitress import serve
 import yaml
+import boto3
+import botocore
 
 from flask_project.app.my_project import create_app
 
@@ -20,13 +22,29 @@ PRODUCTION = "production"
 FLASK_ENV = "FLASK_ENV"
 ADDITIONAL_CONFIG = "ADDITIONAL_CONFIG"
 
+
+def get_parameter_from_ssm(parameter_name):
+    ssm = boto3.client('ssm', region_name="eu-north-1")
+    try:
+        param = ssm.get_parameter(Name=parameter_name, WithDecryption=True)
+        return param["Parameter"]["Value"]
+    except botocore.exceptions.ClientError as e:
+        print(f"Error fetching parameter {parameter_name} from SSM: {e}")
+        return None
+
 if __name__ == '__main__':
     flask_env = os.environ.get(FLASK_ENV, DEVELOPMENT).lower()
-    config_yaml_path = os.path.join(os.getcwd(), 'config', 'app_ec2.yml')
+    config_yaml_path = os.path.join(os.getcwd(), 'config', 'app.yml')
 
     with open(config_yaml_path, "r", encoding='utf-8') as yaml_file:
         config_data_dict = yaml.load(yaml_file, Loader=yaml.FullLoader)
         additional_config = config_data_dict[ADDITIONAL_CONFIG]
+
+        env_config = config_data_dict.get(flask_env)
+
+        env_config["SQLALCHEMY_DATABASE_URI"] = get_parameter_from_ssm("database-link")
+        additional_config["MYSQL_ROOT_USER"] = get_parameter_from_ssm("database-user")
+        additional_config["MYSQL_ROOT_PASSWORD"] = get_parameter_from_ssm("database-password")
 
         if flask_env == DEVELOPMENT:
             config_data = config_data_dict[DEVELOPMENT]
